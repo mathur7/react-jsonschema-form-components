6455bc244f668e89679e4e422bb7e0b0246ba6a6
Ami Mathur <amimathur@MacBook-Pro.local>
2019-03-08 17:37:58 -0800

    pass component to app

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -265,6 +265,8 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        _assets: path.join(__dirname, "../src/assets"),
+        _components: path.join(__dirname, "../src/components"),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
--- src/components/input.js
+++ src/components/input.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Input = (props) => {
   return (
     <input type="text"
